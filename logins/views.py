1c24d8404fda8f0c8c9b02df809fc54f9087f047
hamo-o <yheel369@gmail.com>
2022-11-01 09:49:48 +0900

    fix: admin 수정

--- logins/views.py
+++ logins/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import User
 
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 # Create your views here.
 
 def create(request):
     if request.method == "POST":
         email = request.POST["email"]
         
-        User.objects.create(email=email)
+        user = User.objects.create(email=email)
+        print(user)
+        user.save()
         return redirect("/")
     
     return render(request, template_name = "home.html")
