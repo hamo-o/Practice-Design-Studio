23ac2269962056b76fc27dd9274bd5cc08d200b9
hamo-o <yheel369@gmail.com>
2022-11-15 11:41:32 +0900

    chore: add robots.txt

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,12 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('logins.urls')),
+    path("robots.txt/", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- logins/urls.py
+++ logins/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.views.generic import TemplateView
+
 app_name="logins"
 urlpatterns = [
     path("", views.create, name="create")
